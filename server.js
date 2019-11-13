9410e6a3fc6fe8060cff3dabf2e48c65cc6fd0c3
Eduardo Aviles <eduardoaviles357@yahoo.com>
2019-11-13 08:22:44 -0800

    updated version, added Schema etc..

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect Data Base
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000   ;
 
-app.listen(
+const server = app.listen(
     PORT, () => 
-    console.log(`server up in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`server up in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red.underline);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+})
